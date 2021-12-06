9e0e580d7cc3ea2a605404531b2b11b4fbd7483c
Rahemul Islam <rahemul.islam@northsouth.edu>
2021-12-06 13:21:51 +0600

    demo login test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,13 @@
         </style>
     </head>
     <body class="antialiased">
-        <h1> {{session('user')->name}}</h1>
+        @if(session('user'))
+            <h1> {{session('user')->name}}</h1>
+        
+        @endif
+        <a href="{{url('./Login')}}">Login</a><br>
+        <a href="{{url('./Signup')}}">Signup</a><br>
+        <a href="{{url('./Logout')}}">Logout</a><br>
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
