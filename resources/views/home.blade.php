c06a30a7abb4f199b770af2dfeeee23b7c34c85f
Mashrur Jamil Rafin <mashrur.jamil@northsouth.edu>
2021-12-09 20:39:06 +0600

    Home page Added

---
+++ resources/views/home.blade.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo</title>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <script
+      src="https://kit.fontawesome.com/64d58efce2.js"
+      crossorigin="anonymous"></script>
+      
+      <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+     
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+
+
+      <script
+      src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js">
+      </script>
+
+    
+    <style>
+        .gol-img
+        {
+            border-radius: 50%;
+            width: 100px;
+            height: 100px;
+        }
+
+    </style>
+
+    <!-- <script src="./css/home.css" defer> </script> -->
+
+</head>
+<body>
+
+    <div class="container">
+
+        <div class="row pt-5">
+            <div class="col-lg-12">
+                <div class="card">
+                    <!-- <img class="card-img-top" src="..." alt="Card image cap"> --}} -->
+                    <div class="card-header text-center"><h5>User Details</h5></div>
+                    <div class="card-body text-center">
+                        <img  class="card-img-top gol-img" src="https://icon-library.com/images/default-profile-icon/default-profile-icon-1.jpg" alt="Avatar"><br>
+                        <a href="#"><button type="button" class="mt-2 btn btn-light btn-sm">Edit Profile</button></a>
+                      <h3 class="pt-2">Mashrur Jamil Rafin</h3>
+                      <h4>Web Developer</h3>
+                      <div class="pt-3">
+                        <a href=""><button type="button" class="btn btn-primary">Start New Project</button></a>
+                        <a href=""><button type="button" class="btn btn-primary">Set Weekly Target</button></a>
+                        <a href=""><button type="button" class="btn btn-primary">Daily Progress</button></a>
+                        </div>
+                    </div>
+                </div> 
+            </div>
+            <div class="col-lg-1">
+                
+            </div>
+        </div>
+
+
+        <div class="row pt-3">
+            <div class="col-lg-6">
+                <div class="card">
+                    <!-- <img class="card-img-top" src="..." alt="Card image cap"> --}} -->
+                    <div class="card-header text-center"><h5>Achievements 🏆</h5></div>
+                    <div class="card-body">
+                        <h6>Sample Achievement lorem ipsum </h6>
+                        <h6>Random Achievement lorem ipsum </h6>
+                        <h6>Sample Achievement lorem ipsum </h6>
+                    </div>
+                </div> 
+            </div>
+            <div class="col-lg-6">
+
+            </div>
+        </div>
+        
+
+
+
+
+
+
+
+
+    </div>
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+
+</body>
+</html>
