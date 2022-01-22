bcad9002bc7dccb2b11b1b2dac5dc7d398e96c63
Mashrur Jamil Rafin <mashrur.jamil@northsouth.edu>
2022-01-22 11:07:00 +0600

    All projects Page Added

---
+++ resources/views/allProjects.blade.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo</title>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <script
+      src="https://kit.fontawesome.com/64d58efce2.js"
+      crossorigin="anonymous"></script>
+      
+      <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+     
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+      
+    
+    <style>
+
+
+    </style>
+
+</head>
+<body>
+    
+    <div class="container">
+        
+
+     <div class="row mt-5">
+        <h3 class="text-center pb-4">All Projects</h3>
+        <div class="col-4">
+            <div class="card"> 
+                <div class="card-body">
+                    <a href="projectOverview.blade.php"><h6 class="text-dark">Self Activity Tracker</h6></a>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore veniam beatae quod delectus voluptates corrupti..</p>
+                    <small><p class="text-muted">1 day Ago</p></small>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-4">
+            <div class="card">
+                <div class="card-body">
+                    <a href=""><h6 class="text-dark">ToDo App</h6></a>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore veniam beatae quod delectus voluptates corrupti..</p>
+                    <small><p class="text-muted">3 days Ago</p></small>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-4">
+            <div class="card">
+                <div class="card-body">
+                    <a href="projectOverview.blade.php"><h6 class="text-dark">Online eBook Maker</h6></a>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore veniam beatae quod delectus voluptates corrupti..</p>
+                    <small><p class="text-muted">5 days Ago</p></small>
+                </div>
+            </div>
+        </div>    
+     </div>
+
+
+     <div class="row pt-2">
+        <div class="col-4">
+            <div class="card">
+                <div class="card-body">
+                    <a href=""><h6 class="text-dark">TicTacToe</h6></a>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore veniam beatae quod delectus voluptates corrupti..</p>
+                    <small><p class="text-muted">7 days Ago</p></small>
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="card">
+                <div class="card-body">
+                    <a href=""><h6 class="text-dark">Medical App</h6></a>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore veniam beatae quod delectus voluptates corrupti..</p>
+                    <small><p class="text-muted">9 days Ago</p></small>
+                    
+                </div>
+            </div>
+        </div>
+
+        <div class="col-4">
+            <div class="card">
+                <div class="card-body">
+                    <a href="projectOverview.blade.php"><h6 class="text-dark">Online eBook Maker</h6></a>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore veniam beatae quod delectus voluptates corrupti..</p>
+                    <small><p class="text-muted">12 days Ago</p></small>
+                </div>
+            </div>
+        </div>
+
+
+
+    </div>
+
+
+
+
+
+
+
+
+    </div>
+ 
+    
+    
+    
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+
+</body>
+</html>
