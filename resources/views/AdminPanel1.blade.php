35d828b26d78e1a8eb32603f6f126fc9fc8efad4
Mashrur Jamil Rafin <mashrur.jamil@northsouth.edu>
2022-02-07 22:38:31 +0600

    Individual & Overview Graphs added

--- resources/views/AdminPanel1.blade.php
+++ resources/views/AdminPanel1.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Demo</title>
+    <title>Admin</title>
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <script
       src="https://kit.fontawesome.com/64d58efce2.js"
@@ -18,7 +18,7 @@
       src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js">
       </script>
 
-      <!-- Slider JS -->
+      <!-- Swiper JS -->
       <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css"/>
       <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
       
@@ -45,6 +45,25 @@
           padding: 8px 8px 4px 8px;
           margin-right: 3px;
           margin-top: 3px;
+        }
+
+        .bx-2{
+        
+          float: center;
+          text-align: center;
+          font-size: 10px;
+          padding: 5px 5px 0px 5px;
+          margin-bottom: -10px;
+          margin-top: -5px;
+          margin-right: 2px;
+          font-weight: bold;
+          
+        }
+        
+        .bx-2-txt{
+          border: #002db3 1px solid;
+          float: right;
+          padding: 3px;
         }
         
 
@@ -94,26 +113,45 @@
 
 
    <div class="bx mb-5">
-    <div class="card-title mt-3" style="padding-left: 15px; border-left: 4px solid blue;"><h4>Individual Team Report (<span>January, 2022</span>)</h4>
+    <div class="card-title mt-3" style="padding-left: 15px; border-left: 4px solid blue;"><h4 class="mb-4">Individual Team Report (<span>January, 2022</span>)</h4>
     </div>
     <div class="row p-2">
       <div class="col-4">
         <div class="card">
-          <div class="card-title text-center pt-2"><h6>Web</h6></div>
-          <canvas id="myHoriBarChart" style="width:100%;"></canvas>
+          <div class="card-title text-center pt-3" style="color:#002db3;"><h6>Web</h6></div>
+          <div class="bx-2"> <p  class="bx-2-txt"style="color: #002db3;">Total Working Hour &emsp;: <span>333</span> hrs<br> 
+            &nbsp;Avg Working Hour &emsp;&nbsp;&nbsp;: <span>11.1</span> hrs</p></div>
+          <canvas id="webBar" style="width:100%;"></canvas>
         </div>
       </div>
       <div class="col-4">
-        <div class="card-header">Apps</div> 
+        <div class="card">
+          <div class="card-title text-center pt-3" style="color:#002db3;"><h6>Apps</h6></div>
+          <div class="bx-2"> <p  class="bx-2-txt"style="color: #002db3;">Total Working Hour &emsp;: <span>339</span> hrs<br> 
+            &nbsp;Avg Working Hour &emsp;&nbsp;&nbsp;: <span>11.3</span> hrs</p></div>
+          <canvas id="appBar" style="width:100%;"></canvas>
+        </div>
       </div>
       <div class="col-4">
-        <div class="card-header">Graphic Design</div>
+        <div class="card">
+          <div class="card-title text-center pt-3" style="color:#002db3;"><h6>Graphic Design</h6></div>
+          <div class="bx-2"> <p  class="bx-2-txt"style="color: #002db3;">Total Working Hour &emsp;: <span>465</span> hrs<br> 
+            &nbsp;Avg Working Hour &emsp;&nbsp;&nbsp;&nbsp;: <span>15.5</span> hrs</p></div>
+          <canvas id="graphicBar" style="width:100%;"></canvas>
+        </div>
       </div>
     </div>
-  
-
 </div>
 
+
+
+        <div class="bx mb-5">
+          <div class="card-title mt-3" style="padding-left: 15px; border-left: 4px solid blue;"><h4>Work Overview</h4>
+          </div>
+          <canvas id="StackedBar-Chart" style="width:100%;"></canvas>
+        
+
+        </div>
 
 
 
@@ -329,7 +367,7 @@
             return ""; 
           }
 
-        new Chart("myHoriBarChart", {
+        new Chart("webBar", {
         type: "bar",
         data: {
         labels: xValues,
@@ -387,8 +425,214 @@
 
 
 
+          <!-- Apps Chart-->
+          <script>
+            var xValues = ["1", "2", "3", "4", "5", "6", "7", " 8", "9", "", "11", "12",
+          "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31" ];
+            var yValues = [6, 10, 9, 13, 6, 6, 7, 5, 3, 8, 4, 15, 7, 8, 9, 4, 2, 4, 6, 5, 14, 0, 5, 8, 0, 2, 4, 7, 4, 4, 6];
+            var barColors = "#4d4dff";
+  
+            
+           //made the tooltip title blank  
+            const Titletooltip1 = (tooltipItems) => {
+              return ""; 
+            }
+  
+          new Chart("appBar", {
+          type: "bar",
+          data: {
+          labels: xValues,
+          datasets: [{
+          backgroundColor: barColors,
+          data: yValues
+          }]
+          },
+  
+          options:{
+  
+          legend:{
+            display: false,
+          },
+  
+          title: {
+              display: false,
+              text: "Apps",
+              fontSize: 20
+          },
+  
+          scales: {
+          yAxes: [{
+              ticks: {
+                  beginAtZero: true,
+                  suggestedMin: 0, 
+                  suggestedMax: 15,
+                  //callback: function(value){
+                  //return value+ " hrs"; //For unit hrs
+                  //}
+                  
+              }
+          }]
+  
+      },
+  
+          
+          tooltips: {
+          enabled: true,
+          mode: 'single',
+          displayColors:true,
+          callbacks: {
+            title: Titletooltip1,
+           label: function(tooltipItems, data) {
+           return 'Day '+ tooltipItems.xLabel + ': ' + tooltipItems.yLabel + ' Hrs';
+           }
+       }
+   }
+          },
+  
+          
+  
+          });
+          </script>
 
 
+
+
+            <!-- Graphics Chart-->
+            <script>
+              var xValues = ["1", "2", "3", "4", "5", "6", "7", " 8", "9", "", "11", "12",
+            "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31" ];
+              var yValues = [9, 7, 5, 15, 3, 8, 9, 5, 2, 0, 0, 7, 3, 6, 9, 4, 3, 7, 9, 5, 2, 0, 5, 0, 1, 2, 4, 7, 5, 3, 4];
+              var barColors = "#4d4dff";
+
+              
+            //made the tooltip title blank  
+              const Titletooltip2 = (tooltipItems) => {
+                return ""; 
+              }
+
+            new Chart("graphicBar", {
+            type: "bar",
+            data: {
+            labels: xValues,
+            datasets: [{
+            backgroundColor: barColors,
+            data: yValues
+            }]
+            },
+
+            options:{
+
+            legend:{
+              display: false,
+            },
+
+            title: {
+                display: false,
+                text: "Graphics",
+                fontSize: 20
+            },
+
+            scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                    suggestedMin: 0, 
+                    suggestedMax: 15,
+                    //callback: function(value){
+                    //return value+ " hrs"; //For unit hrs
+                    //}
+                    
+                }
+            }]
+
+            },
+
+
+            tooltips: {
+            enabled: true,
+            mode: 'single',
+            displayColors:true,
+            callbacks: {
+              title: Titletooltip2,
+            label: function(tooltipItems, data) {
+            return 'Day '+ tooltipItems.xLabel + ': ' + tooltipItems.yLabel + ' Hrs';
+            }
+            }
+            }
+            },
+
+
+
+            });
+            </script>
+
+
+              <script>
+                var config = {
+              type: 'bar',
+              data: {
+                  labels: ["Web", "Apps", "Graphic Design"],
+                  datasets: [{
+                      label: "General Projects",
+                      backgroundColor: "rgba(16, 94, 172, 1.0)",
+                      data: [25, 50, 30],
+                      barThickness: 15,
+                      borderRadius: 15,
+                  }, {
+                      label: "Collabaration Works",
+                      backgroundColor: "rgba(247, 127, 7, 1.0)",
+                      data: [50, 5, 22],
+                      barThickness: 15,
+                      borderRadius: 15,
+                  }, {
+                      label: "Mega Projects",
+                      backgroundColor: "#dbe2fc",
+                      data: [100, 100, 100],
+                      barThickness: 15,
+                      borderRadius: 15,
+                  },]
+              },
+              options: {
+                  legend: {
+                      display: true,
+                        position: 'bottom',
+                  },
+                  // responsive: true,
+                  scales: {
+                      xAxes: [{
+                          //stacked: true,
+                          ticks: {
+                              //min: 0,
+                             //max: 100,
+                              //suggestedMin: 0, 
+                              //suggestedMax: 15,
+                          },
+                          gridLines: {
+                          display:true
+                        }
+                      }],
+                      yAxes: [{
+                          //stacked: true,
+                          ticks: {
+                              //min: 0,
+                              stepSize: 25,
+                              //max: 100,
+                          },
+                          gridLines: {
+                          display:true
+                        }
+
+                      }]
+                  },
+                  tooltips: {
+                      enabled: true,
+                      mode: 'single',
+                  }
+              }
+              };
+              var ctx = document.getElementById("StackedBar-Chart").getContext("2d");
+              new Chart(ctx, config);
+              </script>
 
 
 
