42e98b22f5c5dceb602bf4788ee71a181748a48b
Mashrur Jamil Rafin <mashrur.jamil@northsouth.edu>
2022-02-13 22:49:12 +0600

    Admin Daily Attendance page created

---
+++ resources/views/dailyAttendance.blade.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo</title>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <script
+      src="https://kit.fontawesome.com/64d58efce2.js"
+      crossorigin="anonymous"></script>
+      
+      <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+     
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+      
+    
+    <style>
+
+
+    </style>
+
+</head>
+<body>
+    
+    <div class="container">
+
+        <h3 class="text-center pt-3 pb-3">Daily Attendance</h3>
+        <table class="table table-hover table-bordered text-center">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Designation</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Mashrur Jamil Rafin</td>
+                <td>Junior Web Developer</td>
+                <td class="text-danger">Absent</td>
+              <tr>
+                <td>Rahemul Islam</td>
+                <td>Junior Web Developer</td>
+                <td class="text-success">Present</td>
+              </tr>
+              <tr>
+                <td>Mahfuj Ahmed Jim</td>
+                <td>Senior Mobile App Developer</td>
+                <td class="text-success">Present</td>
+              </tr>
+              <tr>
+                <td>Hasibul Islam Shanto</td>
+                <td>Graphic Designer</td>
+                <td class="text-success">Present</td>
+              </tr>
+              <tr>
+                <td>Tawsif Mahmud</td>
+                <td>Junior Software Engineer</td>
+                <td class="text-success">Present</td>
+              </tr>
+            </tbody>
+          </table>
+
+
+
+
+
+
+
+    </div>
+ 
+    
+    
+    
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+
+</body>
+</html>
