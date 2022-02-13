bc6d58dda3ac0afa265d7b230d674aede4339aaa
Mashrur Jamil Rafin <mashrur.jamil@northsouth.edu>
2022-02-13 21:47:32 +0600

    Personality result page created

--- resources/views/personalityResult.blade.php
+++ resources/views/personalityResult.blade.php
@@ -24,8 +24,42 @@
     
     <div class="container">
 
-    <p>Your Personality is ABCD.</p>
+    <div class="row">
+      <div class="col-lg-6">
 
+        <img src="personality.svg" style="padding-top:30%; width: 100%;" alt="">
+
+      </div>
+      <div class="col-lg-6">
+        <h4 class="text-center pt-5">Your Personality is <span class="text-primary">INTP</span></h4>
+        <h5 class="text-center pt-2">To learn more- <a href="https://www.youtube.com/watch?v=bfusRr20RWU" target="_blank">Click here</a></h5>
+
+
+        <h5 class="text-center pt-3 fw-bold">Personality traits explained:</h5>
+        <span class="fw-bold">Mind:</span> <br>
+        <span class="fw-bold">Introverted (I) -</span> prefer solitary activities, think before speaking, get
+          exhausted by social interaction. <br>
+          <span class="fw-bold">Extraverted (E) -</span> prefer group activities, think while speaking, get energized
+          by social interaction. <br> <br> 
+          <span class="fw-bold">Energy: </span><br>
+          <span class="fw-bold">Intuitive (N) -</span>  imaginative, rely on their intuition, absorbed in ideas, focus
+          on what might happen. <br>
+          <span class="fw-bold">Sensing (S) -</span>  down-to-earth, rely on their senses, absorbed in practical
+          matters, focus on what has happened. <br> <br>
+          <span class="fw-bold">Nature: </span> <br>
+          <span class="fw-bold">Thinking (T) - </span>tough, follow their minds, focus on objectivity and rationality. <br>
+          <span class="fw-bold">Feeling (F) - </span> sensitive, follow their hearts, focus on harmony and
+          cooperation. <br> <br>
+          <span class="fw-bold">Tactics: </span><br>
+          <span class="fw-bold">Judging (J) -</span> decisive, prefer clear rules and guidelines, see deadlines as
+          sacred, seek closure. <br>
+          <span class="fw-bold">Perceiving (P) -</span> very good at improvising, prefer keeping their options open,
+          relaxed about their work, seek freedom. <br>
+    
+        
+      </div>
+    </div>
+   
 
 
 
