e2f489d0b62abb9aa349237eeb31d50ce6f4ff39
Mashrur Jamil Rafin <mashrur.jamil@northsouth.edu>
2022-02-15 22:32:55 +0600

    Landing page created

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -16,14 +16,29 @@
     
     <style>
 
-
     </style>
 
 </head>
 <body>
-    
-    <div class="container">
 
+    <div class="container">
+        <div class="row pt-5">
+            <div class="col-lg-6">
+                <div class="text-center" style="padding-top: 10%">
+                    <img src="../../public/image/SAT-w1.png" width="35%" class="d-inline-block align-top pt-2" alt="">
+                <h1><span style="color:#002db3">Track</span> your work<br>
+                and be <span style="color:#002db3">productive</span></h1>
+                <p class="pt-1">Self Activity Tracker helps you to plan the activites you want to do and helps to keep track of your tasks. Collaborate with your co-workers and do productive things together!
+                </p>
+                <button class="btn btn-lg" style="background: #002db3; color:white;">Login</button>
+                <p class="pt-3">New in Accelerating Intelligence?&nbsp;&nbsp;<a href="" style="text-decoration: none; color:#002db3">Register</a></p>
+                </div>    
+                   
+            </div>
+            <div class="col-lg-6">
+                <img src="../../public/image/landing1.svg" style="width:100%; display: block; margin-left: auto;margin-right: auto;" alt="">
+            </div>
+        </div>
 
 
 
