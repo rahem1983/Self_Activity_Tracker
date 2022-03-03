cfbc829c5abab95b494c907b78e27eb794ecd4b9
Rahemul Islam <rahemul.islam@northsouth.edu>
2022-03-04 03:42:56 +0600

    ITR graph modified

--- public/js/admin_1.js
+++ public/js/admin_1.js
@@ -277,219 +277,219 @@ $.ajax({
         }
     }
 });
-        // <!-- Web Chart Start-->
+//         // <!-- Web Chart Start-->
         
-        var xValues = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12",
-        "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31" ];
-        var yValues = [6, 10, 9, 12, 2, 6, 7, 5, 0, 0, 4, 7, 4, 6, 9, 3, 2, 4, 6, 5, 0, 0, 5, 8, 1, 2, 4, 7, 5, 3, 4];
-        var barColors = "#4d4dff";
+//         var xValues = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12",
+//         "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31" ];
+//         var yValues = [6, 10, 9, 12, 2, 6, 7, 5, 0, 0, 4, 7, 4, 6, 9, 3, 2, 4, 6, 5, 0, 0, 5, 8, 1, 2, 4, 7, 5, 3, 4];
+//         var barColors = "#4d4dff";
 
         
-        //made the tooltip title blank  
-        const Titletooltip = (tooltipItems) => {
-            return ""; 
-        }
+//         //made the tooltip title blank  
+//         const Titletooltip = (tooltipItems) => {
+//             return ""; 
+//         }
 
-        new Chart("webBar", {
-        type: "bar",
-        data: {
-        labels: xValues,
-        datasets: [{
-        backgroundColor: barColors,
-        data: yValues
-        }]
-        },
+//         new Chart("webBar", {
+//         type: "bar",
+//         data: {
+//         labels: xValues,
+//         datasets: [{
+//         backgroundColor: barColors,
+//         data: yValues
+//         }]
+//         },
 
-        options:{
+//         options:{
 
-        legend:{
-        display: false,
-        },
+//         legend:{
+//         display: false,
+//         },
 
-        title: {
-            display: false,
-            text: "Web",
-            fontSize: 20
-        },
+//         title: {
+//             display: false,
+//             text: "Web",
+//             fontSize: 20
+//         },
 
-        scales: {
-        yAxes: [{
-            ticks: {
-                beginAtZero: true,
-                suggestedMin: 0, 
-                suggestedMax: 15,
-                //callback: function(value){
-                //return value+ " hrs"; //For unit hrs
-                //}
+//         scales: {
+//         yAxes: [{
+//             ticks: {
+//                 beginAtZero: true,
+//                 suggestedMin: 0, 
+//                 suggestedMax: 15,
+//                 //callback: function(value){
+//                 //return value+ " hrs"; //For unit hrs
+//                 //}
                 
-            }
-        }]
+//             }
+//         }]
 
-        },
-
-
-        tooltips: {
-        enabled: true,
-        mode: 'single',
-        displayColors:true,
-        callbacks: {
-        title: Titletooltip,
-        label: function(tooltipItems, data) {
-        return 'Day '+ tooltipItems.xLabel + ': ' + tooltipItems.yLabel + ' Hrs';
-        }
-        }           
-        }
-        },
+//         },
 
 
+//         tooltips: {
+//         enabled: true,
+//         mode: 'single',
+//         displayColors:true,
+//         callbacks: {
+//         title: Titletooltip,
+//         label: function(tooltipItems, data) {
+//         return 'Day '+ tooltipItems.xLabel + ': ' + tooltipItems.yLabel + ' Hrs';
+//         }
+//         }           
+//         }
+//         },
 
-        });
-
-        // <!-- Web Chart End-->
 
 
-// <!-- Apps Chart Start-->
+//         });
 
-            var xValues = ["1", "2", "3", "4", "5", "6", "7", " 8", "9", "10", "11", "12",
-          "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31" ];
-            var yValues = [6, 10, 9, 13, 6, 6, 7, 5, 3, 8, 4, 15, 7, 8, 9, 4, 2, 4, 6, 5, 14, 0, 5, 8, 0, 2, 4, 7, 4, 4, 6];
-            var barColors = "#4d4dff";
+//         // <!-- Web Chart End-->
+
+
+// // <!-- Apps Chart Start-->
+
+//             var xValues = ["1", "2", "3", "4", "5", "6", "7", " 8", "9", "10", "11", "12",
+//           "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31" ];
+//             var yValues = [6, 10, 9, 13, 6, 6, 7, 5, 3, 8, 4, 15, 7, 8, 9, 4, 2, 4, 6, 5, 14, 0, 5, 8, 0, 2, 4, 7, 4, 4, 6];
+//             var barColors = "#4d4dff";
   
             
-           //made the tooltip title blank  
-            const Titletooltip1 = (tooltipItems) => {
-              return ""; 
-            }
+//            //made the tooltip title blank  
+//             const Titletooltip1 = (tooltipItems) => {
+//               return ""; 
+//             }
   
-          new Chart("appBar", {
-          type: "bar",
-          data: {
-          labels: xValues,
-          datasets: [{
-          backgroundColor: barColors,
-          data: yValues
-          }]
-          },
+//           new Chart("appBar", {
+//           type: "bar",
+//           data: {
+//           labels: xValues,
+//           datasets: [{
+//           backgroundColor: barColors,
+//           data: yValues
+//           }]
+//           },
   
-          options:{
+//           options:{
   
-          legend:{
-            display: false,
-          },
+//           legend:{
+//             display: false,
+//           },
   
-          title: {
-              display: false,
-              text: "Apps",
-              fontSize: 20
-          },
+//           title: {
+//               display: false,
+//               text: "Apps",
+//               fontSize: 20
+//           },
   
-          scales: {
-          yAxes: [{
-              ticks: {
-                  beginAtZero: true,
-                  suggestedMin: 0, 
-                  suggestedMax: 15,
-                  //callback: function(value){
-                  //return value+ " hrs"; //For unit hrs
-                  //}
+//           scales: {
+//           yAxes: [{
+//               ticks: {
+//                   beginAtZero: true,
+//                   suggestedMin: 0, 
+//                   suggestedMax: 15,
+//                   //callback: function(value){
+//                   //return value+ " hrs"; //For unit hrs
+//                   //}
                   
-              }
-          }]
+//               }
+//           }]
   
-      },
-  
-          
-          tooltips: {
-          enabled: true,
-          mode: 'single',
-          displayColors:true,
-          callbacks: {
-            title: Titletooltip1,
-           label: function(tooltipItems, data) {
-           return 'Day '+ tooltipItems.xLabel + ': ' + tooltipItems.yLabel + ' Hrs';
-           }
-       }
-   }
-          },
+//       },
   
           
+//           tooltips: {
+//           enabled: true,
+//           mode: 'single',
+//           displayColors:true,
+//           callbacks: {
+//             title: Titletooltip1,
+//            label: function(tooltipItems, data) {
+//            return 'Day '+ tooltipItems.xLabel + ': ' + tooltipItems.yLabel + ' Hrs';
+//            }
+//        }
+//    }
+//           },
   
-          });
+          
+  
+//           });
 
- // <!-- Apps Chart End--> 
+//  // <!-- Apps Chart End--> 
  
  
 
-        // <!-- Graphics Chart Start-->
+//         // <!-- Graphics Chart Start-->
 
-        var xValues = ["1", "2", "3", "4", "5", "6", "7", " 8", "9", "10", "11", "12",
-        "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31" ];
-        var yValues = [9, 7, 5, 15, 3, 8, 9, 5, 2, 0, 0, 7, 3, 6, 9, 4, 3, 7, 9, 5, 2, 0, 5, 0, 1, 2, 4, 7, 5, 3, 4];
-        var barColors = "#4d4dff";
+//         var xValues = ["1", "2", "3", "4", "5", "6", "7", " 8", "9", "10", "11", "12",
+//         "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31" ];
+//         var yValues = [9, 7, 5, 15, 3, 8, 9, 5, 2, 0, 0, 7, 3, 6, 9, 4, 3, 7, 9, 5, 2, 0, 5, 0, 1, 2, 4, 7, 5, 3, 4];
+//         var barColors = "#4d4dff";
 
         
-        //made the tooltip title blank  
-        const Titletooltip2 = (tooltipItems) => {
-            return ""; 
-        }
+//         //made the tooltip title blank  
+//         const Titletooltip2 = (tooltipItems) => {
+//             return ""; 
+//         }
 
-        new Chart("graphicBar", {
-        type: "bar",
-        data: {
-        labels: xValues,
-        datasets: [{
-        backgroundColor: barColors,
-        data: yValues
-        }]
-        },
+//         new Chart("graphicBar", {
+//         type: "bar",
+//         data: {
+//         labels: xValues,
+//         datasets: [{
+//         backgroundColor: barColors,
+//         data: yValues
+//         }]
+//         },
 
-        options:{
+//         options:{
 
-        legend:{
-        display: false,
-        },
+//         legend:{
+//         display: false,
+//         },
 
-        title: {
-            display: false,
-            text: "Graphics",
-            fontSize: 20
-        },
+//         title: {
+//             display: false,
+//             text: "Graphics",
+//             fontSize: 20
+//         },
 
-        scales: {
-        yAxes: [{
-            ticks: {
-                beginAtZero: true,
-                suggestedMin: 0, 
-                suggestedMax: 15,
-                //callback: function(value){
-                //return value+ " hrs"; //For unit hrs
-                //}
+//         scales: {
+//         yAxes: [{
+//             ticks: {
+//                 beginAtZero: true,
+//                 suggestedMin: 0, 
+//                 suggestedMax: 15,
+//                 //callback: function(value){
+//                 //return value+ " hrs"; //For unit hrs
+//                 //}
                 
-            }
-        }]
+//             }
+//         }]
 
-        },
-
-
-        tooltips: {
-        enabled: true,
-        mode: 'single',
-        displayColors:true,
-        callbacks: {
-        title: Titletooltip2,
-        label: function(tooltipItems, data) {
-        return 'Day '+ tooltipItems.xLabel + ': ' + tooltipItems.yLabel + ' Hrs';
-        }
-        }
-        }
-        },
+//         },
 
 
+//         tooltips: {
+//         enabled: true,
+//         mode: 'single',
+//         displayColors:true,
+//         callbacks: {
+//         title: Titletooltip2,
+//         label: function(tooltipItems, data) {
+//         return 'Day '+ tooltipItems.xLabel + ': ' + tooltipItems.yLabel + ' Hrs';
+//         }
+//         }
+//         }
+//         },
 
-        });
 
 
-        // <!-- Graphics Chart End-->
+//         });
+
+
+//         // <!-- Graphics Chart End-->
 
 
 
